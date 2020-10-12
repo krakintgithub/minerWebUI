2a5ba28995752bd73fb2e92c4757c5c364200959
coka <krakint@protonmail.com>
2020-10-12 19:33:48 -0400

    new desing

--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,64 @@
 <template>
-  <div class="container">
 
-    <div class="header">
-      <img src="./assets/img/cave_header.svg" width="100%">
-    </div>
 
-    <div class="b-row moveUp leftPadding">
-      <div>
-        <div class="logo_text">
-          <h1>
-            <img src="./assets/img/logo_text.png" width="25%">
-          </h1>
+  <div class="hasmetamask">
+    <div class="popup" id="popup">
+      <div class="popup-inner">
+
+
+        <div class="miner">
+          <img src="./assets/img/miner.png" alt="" width="100%" class="miner">
         </div>
-        <hr>
-        <hr>
-        <hr>
-        <div class="krakintext">
-          <div class="welcome">Welcome to Krakin't miner !</div>
-          This simple interface will let you mine the Krakin't tokens by making an initial deposit.
-          Mechanism is very similar to staking, however, we have also added <i>Proof Of Burn</i> to improve the price vs demand precision.
-          Please use and install <a href="https://metamask.io/download.html" target="_blank">Metamask</a>.
-          Once Krakin't tokens are deposited, simply sit back and watch your earnings grow every 20 seconds or less in a Miner Console.
+      </div>
+
+      <!--        <div class="install-metamask">Please install:<br><img src="./assets/img/metamask_logo.png" height="50px"><br>-->
+      <!--          Metamask.<br> Then, reload...</div>-->
+
+      <div class="depscr" style="display: none">DEPOSIT<br><img src="./assets/img/deposit.png" height="50px">
+        <div style="text-align: left">
+          <li>Enter amount</li>
+          <li>Click green button</li>
         </div>
+      </div>
 
-        <div class="console">
-
-          <table style="width:100%">
-            <tr>
-              <th>
-                <div style="opacity: 0">...........</div>
-              </th>
-              <th></th>
-            </tr>
-            <tr>
-              <td>
-                <div style="opacity: 0">...........</div>
-              </td>
-              <td>
-                MINER CONSOLE<br><br>
-                <div style="color: #008b00; display: inline-block;">Your Balance:</div>
-                {{ reward }}<br><br>
-                <div style="color: #008b00; display: inline-block;">Block Number:</div>
-                {{ blockNumber }} <br><br>
-              </td>
-            </tr>
-          </table>
-
-          <img src="./assets/img/console.png" style='height: 100%; width: 100%; object-fit: contain'>
+      <div class="withscr" style="display: none">WITHDRAW<br><img src="./assets/img/withdraw.png" height="50px">
+        <div style="text-align: left">
+          <li>Enter amount</li>
+          <li>Click red button</li>
         </div>
+      </div>
 
-          <div class="krakintext2">
-            To start mining or increase the mining power, specify Krakin't token amount in a "Deposit KRK" field, and press a "Deposit" button.
-            Do all confirmations in the Metamask popup-window, and wait for the network to approve your transfer. Then simply watch the Miner Console.
-            Repeat the same if you want to obtain a certain amount of tokens back from a miner and into your Metamask wallet.
-            If you don't have any tokens to start with, please refer to <a href="https://www.krakint.com" target="_blank">Krakin't Web-Page</a>.
-          </div>
-          <div class="trade">
-            <table style="width:63%">
-              <tr>
-                <td>
-                  <label for="deposit">
-                    Deposit KRK
-                  </label><br>
-                  <b-form-input
-                      id="deposit"
-                      v-model="deposit"
-                      type="text"
-                      placeholder="Amount"
-                  />
-                  <hr>
+      <!--        <div class="metamask-link"><a href="https://metamask.io/download.html" target="_blank">Click Here</a></div>-->
+      <div class="balance">
+        Miner Balance: <br>
+        <div id="balance_num" style="display: inline-block;">Connect to Metamask</div>
+        <br><br>
+        Block Number: <br>
+        <div id="block_num" style="display: inline-block;">Connect to Metamask</div>
+      </div>
+      <div class="txtform"><input type="text" id="tbox" name="tbox" placeholder="Enter amount"></div>
+      <div class="depbttn" id="depbttn" onmouseenter="displdep()" onmouseleave="mouseExit()">
+        <div class="container">
+          <b-button
+              @click="processDeposit"
+              class="depositButton"
+          >
 
-                  <div>
-                    <b-button
-                        :variant="'primary'"
-                        @click="processDeposit"
-                    >
-                      Deposit
-                    </b-button>
-                    <img
-                        v-show="isLoad"
-                        src="https://media.giphy.com/media/2A6xoqXc9qML9gzBUE/giphy.gif"
-                    >
-                  </div>
-
-
-                </td>
-                <td>
-                  <div style="opacity: 0">......................................</div>
-                </td>
-                <td>
-                  <label for="withdraw">
-                    Withdraw KRK
-                  </label><br>
-                  <b-form-input
-                      id="withdraw"
-                      v-model="withdraw"
-                      type="text"
-                      placeholder="Amount"
-                  />
-                  <hr>
-
-                  <div>
-                    <b-button
-                        :variant="'primary'"
-                        @click="processWithdraw"
-                    >
-                      Withdraw
-                    </b-button>
-                    <img
-                        v-show="isLoad"
-                        src="https://media.giphy.com/media/2A6xoqXc9qML9gzBUE/giphy.gif"
-                    >
-                  </div>
-
-                </td>
-              </tr>
-            </table>
-
-          </div>
-
-
-
+          </b-button>
         </div>
-
-
+      </div>
+      <div class="wthbttn" id="wthbttn" onmouseenter="displwtn()" onmouseleave="mouseExit()">
+        <div class="container">
+          <b-button
+              @click="processWithdraw"
+              class="withdrawButton"
+          >
+          </b-button>
+        </div>
+      </div>
     </div>
-
-
-
-    <div class="krakintext2" style="margin-top: 12vw;">
-      <div>Your Address: {{ userAddress }}</div>
-      <div>Total Burned: {{ totalBurned }} KRK</div>
-      <div>Total Minted: {{ totalMinted }} KRK</div>
-      <div>Number of Miners: {{ miners }}</div>
-    </div>
-
-
-    <div class="footer">
-      <img src="./assets/img/cave_footer.svg" width="100%">
-    </div>
-
   </div>
+
 
 </template>
 
@@ -160,21 +78,19 @@ function formatNumber(x) {
   return x.replace(/,/g, '');
 }
 
-function formatAddress(address){
-  return address.substr(0,5)+"..."+address.substr(35);
+function formatAddress(address) {
+  return address.substr(0, 5) + "..." + address.substr(35);
 }
-
-
 
 
 export default {
   name: 'APP',
-  created: function() {
-    this.interval = setInterval(() => this.refreshData(), 6000);
+  created: function () {
+    this.interval = setInterval(() => this.refreshData(), 3000);
   },
-  data: function() {
+  data: function () {
     return {
-      userAddress: 'Connect to Metamask',
+      userAddress: 'Connect to Metamask, then reload.',
       miners: '-1',
       totalBurned: '-1',
       totalMinted: '-1',
@@ -188,61 +104,50 @@ export default {
     this.refreshData();
   },
   methods: {
-    refreshData(){
+    refreshData() {
       this.userAddress = formatAddress(web3.eth.accounts.givenProvider.selectedAddress);
       const fromAddress = web3.eth.accounts.givenProvider.selectedAddress;
 
-      auctionBox.methods
-          .getPivot()
-          .call()
-          .then((n) => {
-            this.miners = n;
-          });
-      auctionBox.methods
-          .getTotalBurned()
-          .call()
-          .then((n) => {
-            this.totalBurned = numberWithCommas(truncateNumber(web3.utils.fromWei(n, 'ether')));
-          });
-      auctionBox.methods
-          .getTotalMinted()
-          .call()
-          .then((n) => {
-            this.totalMinted = numberWithCommas(truncateNumber(web3.utils.fromWei(n, 'ether')));
-          });
       auctionBox.methods
           .getCurrentBlockNumber()
           .call()
           .then((n) => {
             this.blockNumber = n;
+            document.getElementById("block_num").innerText = this.blockNumber;
           });
       auctionBox.methods
           .showReward(fromAddress)
           .call()
           .then((n) => {
             this.reward = numberWithCommas(truncateNumber(web3.utils.fromWei(n, 'ether')));
+            document.getElementById("balance_num").innerText = this.reward;
           });
     },
 
     processDeposit() {
       const fromAddress = web3.eth.accounts.givenProvider.selectedAddress;
-      const amount = web3.utils.toWei(formatNumber(this.deposit), 'ether');
+      const tboxval = document.getElementById('tbox').value;
+      const amount = web3.utils.toWei(formatNumber(tboxval), 'ether');
       this.deposit = '';
 
       auctionBox.methods
           .mine(amount).send({
         from: fromAddress,
+      }).then(() => {
+        document.getElementById('tbox').value = '';
       });
     },
     processWithdraw() {
-
       const fromAddress = web3.eth.accounts.givenProvider.selectedAddress;
-      const amount = web3.utils.toWei(formatNumber(this.withdraw), 'ether');
+      const tboxval = document.getElementById('tbox').value;
+      const amount = web3.utils.toWei(formatNumber(tboxval), 'ether');
       this.withdraw = '';
 
       auctionBox.methods
           .getReward(amount).send({
         from: fromAddress,
+      }).then(() => {
+        document.getElementById('tbox').value = '';
       });
 
     }
@@ -253,165 +158,25 @@ export default {
 
 <style>
 
-.header {
-  width: 100%;
-  color: white;
-  text-align: center;
-}
-.footer {
-  width: 100%;
-  color: white;
-  text-align: center;
-  left: 0;
-  bottom: 0;
+.depositButton {
+  margin-top: 3px;
+  margin-right: 26px !important;
+  margin-left: -13px;
+  border-radius: 100px !important;
+  height: 70px;
+  width: 69px;
+  background: none !important;
+  border: 0px !important;
 }
 
-.logo_text{
-  text-align: center;
-}
-
-.moveUp{
-  margin-top: -30%;
-}
-
-.leftPadding{
-  padding-left: 20px;
-}
-
-table {
-  border-collapse: collapse;
-  position: absolute;
-  color: #74ff74;
-  margin-top: 25px;
-  font-size: 1.4vw;
-}
-
-th, td {
-  width: 100px;
-  overflow: hidden;
-}
-
-
-h1 {
-  color:#00ff19;
-  text-shadow: rgb(0, 0, 0) 3px 0px 0px, rgb(0, 0, 0) 2.83487px 0.981584px 0px, rgb(0, 0, 0) 2.35766px 1.85511px 0px, rgb(0, 0, 0) 1.62091px 2.52441px 0px, rgb(0, 0, 0) 0.705713px 2.91581px 0px, rgb(0, 0, 0) -0.287171px 2.98622px 0px, rgb(0, 0, 0) -1.24844px 2.72789px 0px, rgb(0, 0, 0) -2.07227px 2.16926px 0px, rgb(0, 0, 0) -2.66798px 1.37182px 0px, rgb(0, 0, 0) -2.96998px 0.42336px 0px, rgb(0, 0, 0) -2.94502px -0.571704px 0px, rgb(0, 0, 0) -2.59586px -1.50383px 0px, rgb(0, 0, 0) -1.96093px -2.27041px 0px, rgb(0, 0, 0) -1.11013px -2.78704px 0px, rgb(0, 0, 0) -0.137119px -2.99686px 0px, rgb(0, 0, 0) 0.850987px -2.87677px 0px, rgb(0, 0, 0) 1.74541px -2.43999px 0px, rgb(0, 0, 0) 2.44769px -1.73459px 0px, rgb(0, 0, 0) 2.88051px -0.838247px 0px;
-}
-
-body {
-  background-color: #050000!important;
-}
-
-label{
-  display: inline-block;
-  margin-bottom: 0.5rem;
-  color: #f8faf7;
-  font-size: 1.3vw;
-  font-weight: bold;
-  -webkit-text-stroke: 1px #5d5a53;
-}
-
-
-.container{
-  opacity: 1.0;
-  max-width: 100% !important;
-  padding-right: 0px !important;
-  padding-left: 0px !important;
-  margin-right: 0px !important;
-  margin-left: 0px !important;
-}
-
-.console{
-  background-color: #000000;
-  text-align: center;
-  border: 4px #00ff19 double;
-  width: 50%;
-  position: relative;
-  margin-left: 23%;
-  border-radius: 25px;
-}
-
-.krakintext{
-  text-align: center;
-  width: 50%;
-  margin-left: 23%;
-  color: #e9f0ee;
-  margin-bottom: 60px;
-  font-size: 1.7vw;
-}
-
-.krakintext2{
-  text-align: center;
-  width: 50%;
-  margin-left: 23%;
-  color: #e9f0ee;
-  margin-bottom: 60px;
-  font-size: 1.3vw;
-  margin-top: 50px;
-}
-
-
-.welcome{
-  font-size: 2.8vw;
-  color: #f7dc69;
-  -webkit-text-stroke-width: 1px;
-  -webkit-text-stroke-color: #e8af21;
-}
-
-.stats{
-  text-align: center;
-  width: 50%;
-  margin-left: 23%;
-color: #fff;
-}
-
-.trade{
-  position: relative;
-  margin-bottom: 333px;
-  margin-left: 23%;
-}
-
-
-h1,
-h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
-
-button {
-  background-color: #008cba;
-  color: white;
-}
-
-button:hover {
-  color: #cacfd7 !important;
-  background-color: #5d5952 !important;;
-  border-color: #cacfd7 !important;
-}
-
-.btn-primary{
-  color: #353130!important;
-  background-color: #cacfd7!important;
-  border-color: #5d5952!important;
-}
-
-.form-control{
-  background-color: #cacfd7!important;
-  max-width:200px;
-  color: #000!important;
-
+.withdrawButton {
+  border: 0px !important;
+  margin-top: 3px;
+  margin-left: -13px;
+  border-radius: 100px !important;
+  height: 69px;
+  width: 69px;
+  background: none !important;
 }
 
 </style>
